fe3c8f4b7feda5aa29f163d893fdbde29cf1fa84
Die <dlibonati@leafnoise.io>
2026-04-19 13:36:25 -0300

    fix: clean jest.setup.ts

--- __tests__/jest.setup.ts
+++ __tests__/jest.setup.ts
@@ -1,20 +1,6 @@
 import "@testing-library/jest-dom";
 
-import { mockLocalStorage } from "@tests/__mocks__/localStorage.mock";
-
 const mockUuidV4 = jest.fn(() => "mocked-uuid-1234");
-
-Object.defineProperty(global, "localStorage", {
-  value: {
-    getItem: mockLocalStorage.getItem,
-    setItem: mockLocalStorage.setItem,
-    removeItem: mockLocalStorage.removeItem,
-    clear: mockLocalStorage.clear,
-    key: mockLocalStorage.key,
-    length: mockLocalStorage.length,
-  },
-  writable: true,
-});
 
 if (typeof DragEvent === "undefined") {
   class MockDragEvent extends Event implements DragEvent {
